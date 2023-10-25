1421079e8799bf2725a800fc547bc9b5d3936a72
Товма Дмитрий <cyplenak@ya.ru>
2023-10-25 20:36:09 +0600

    "1"

--- cats/serializers.py
+++ cats/serializers.py
@@ -3,7 +3,18 @@ from rest_framework import serializers
 from .models import Cat
 
 
-class CatSerializer(serializers.ModelSerializer):
+""" class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
         fields = '__all__'
+ """
+
+""" class CatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color', 'birth_year') """
+
+class CatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cat
+        fields = ('name', 'color', 'birth_year')
--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+""" from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -17,3 +17,79 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+ """
+
+""" from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Cats(APIView):
+    def get(self, request, *args, **kwargs):
+        cats = Cat.objects.get(pk=kwargs['pk'])
+        serializer = CatSerializer(cats)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        cats = Cat.objects.get(pk=kwargs['pk'])
+        serializer = CatSerializer(cats, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, *args, **kwargs):
+        cats = Cat.objects.get(pk=kwargs['pk'])
+        serializer = CatSerializer(cats, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        cats = Cat.objects.get(pk=kwargs['pk'])
+        cats.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+ """
+
+""" from rest_framework import generics
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer  """
+
+from rest_framework import viewsets 
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
